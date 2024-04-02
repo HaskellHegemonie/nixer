3c340885a84b3c1f51f36ee5b2e7af99742d33b2
HaskellHegemonie <znerol@mail.de>
2024-04-02 23:20:06 +0200

    Added Example config from disko github. Played in nix repl

---
+++ playground/disko-hybrid.nix
@@ -0,0 +1,36 @@
+{
+  disko.devices = {
+    disk = {
+      vda = {
+        type = "disk";
+        device = "/dev/vda";
+        content = {
+          type = "gpt";
+          partitions = {
+            boot = {
+              size = "1M";
+              type = "EF02"; # for grub MBR
+            };
+            ESP = {
+              size = "512M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot/efi";
+              };
+            };
+            root = {
+              size = "500M";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
